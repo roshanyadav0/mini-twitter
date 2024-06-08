3acecacc8417544d6bddccaba447724cff555a22
Roshan Yadav <ry9826653@gmail.com>
2024-06-08 22:18:58 +0530

    changed users route

--- Client/my-app/src/Components/Users.js
+++ Client/my-app/src/Components/Users.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import NavBar from './NavBar'
+import UserList from './UserList'
 
 function Users() {
 return (
     <div>
         <NavBar/>
-        
+        <UserList/>
     </div>
 )
 }
